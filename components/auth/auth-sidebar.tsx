a652db44c2db532e83e96dc94fe06cbc79b78ccc
Uzor-Nwachukwu <uzor1997@gmail.com>
2024-01-17 14:58:43 +0100

    Fix: logo breaking auth sidebar

--- components/auth/auth-sidebar.tsx
+++ components/auth/auth-sidebar.tsx
@@ -11,7 +11,7 @@ export const AuthSidebar = () => {
           Save
         </h2>
         <Image
-          src="/think-save.png"
+          src="/assets/images/think-save.png"
           alt="Think Save"
           width={200}
           height={200}
