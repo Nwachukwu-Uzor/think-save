92ae6580ae495e755d4f4b35f4889040756a6d09
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-17 08:28:10 +0100

    Fix: background image an color on register and login screens

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,14 +9,14 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
         <AuthSidebar />
       </aside>
       <main className="lg:col-span-6 min-h-screen">
-        <section>{children}</section>
-        <div className="lg:hidden fixed bottom-0 left-0 right-0 px-1 -z-10">
+        <section className="bg-white">{children}</section>
+        <div className="lg:hidden fixed bottom-0 left-0 right-0 px-1 z-10">
           <Image
-            src="/think-save-blue.png"
+            src="/assets/images/think-save-blue.png"
             alt="Think Save"
             width={200}
             height={200}
-            className="w-full m-0 h-full"
+            className="w-full m-0 h-24"
           />
         </div>
       </main>
--- components/shared/button.tsx
+++ components/shared/button.tsx
@@ -5,7 +5,8 @@ const buttonColors = {
   "light-red":
     "bg-main-red-accent text-main-red active:ring active:ring-main-red-accent",
   "white-red": "bg-white text-main-red active:ring active:ring-white",
-  "accent-blue": "bg-accent-blue text-main-blue active:ring active:ring-accent-blue",
+  "accent-blue":
+    "bg-accent-blue text-main-blue active:ring active:ring-accent-blue",
   "main-blue": "bg-main-blue text-white active:ring active:ring-main-blue",
 };
 
@@ -16,7 +17,7 @@ type Props = {
 export const Button: React.FC<Props> = ({ children, color }) => {
   return (
     <button
-      className={`py-1 flex items-center justify-center gap-1 xl:py-2 px-2 lg:px-4 w-full rounded-md font-semibold hover:opacity-75 duration-200 text-sm xl:text-base ${buttonColors[color]}`}
+      className={`flex items-center justify-center gap-1 py-2 px-2 lg:px-4 w-full rounded-md font-semibold hover:opacity-75 duration-200 text-sm xl:text-base ${buttonColors[color]}`}
     >
       {children}
     </button>
