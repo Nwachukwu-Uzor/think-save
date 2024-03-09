b4fbee195cd72f23c9a7b6ebe63275967fd2bc26
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-03-09 07:39:53 +0100

    add auto logout provider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,21 @@
+import { AutoLogoutProvider } from "@/components/Layout/auto-logout-provider";
 import { MainHeader } from "@/components/main/";
 import { Container } from "@/components/shared/";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col min-h-[100vh]">
-      <header className="bg-white">
-        <Container>
-          <MainHeader />
-        </Container>
-      </header>
-      <section className="bg-accent-blue flex-1 py-4 lg:py-6">
-        {children}
-      </section>
-    </main>
+    <AutoLogoutProvider requireSession={true}>
+      <main className="flex flex-col min-h-[100vh]">
+        <header className="bg-white">
+          <Container>
+            <MainHeader />
+          </Container>
+        </header>
+        <section className="bg-accent-blue flex-1 py-4 lg:py-6">
+          {children}
+        </section>
+      </main>
+    </AutoLogoutProvider>
   );
 };
 
